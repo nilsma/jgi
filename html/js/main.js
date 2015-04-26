19d893f9534cf147acda0eb38f9cf09b8fc5d6e3
Nils Martinussen <nilsma231@gmail.com>
2015-04-26 18:13:59 +0200

    re-fixed safari bug where div.details were not displayed properly due to positioning

--- html/js/main.js
+++ html/js/main.js
@@ -2,10 +2,10 @@ $(document).ready(function() {
     $("td.toggleDetails").mouseenter(function() {
         $(this).parent().children(3).children("div").css('display', 'block');
         $(this).parent().children(3).children("div").css('z-index', '2');
-//        $(this).parent().css('position', 'relative');
+        $(this).parent().css('position', 'relative');
     }).mouseleave(function() {
         $(this).parent().children(3).children("div").css('display', 'none');
         $(this).parent().children(3).children("div").css('z-index', '-1');
-//        $(this).parent().css('position', 'static');
+        $(this).parent().css('position', 'static');
     });
 });
